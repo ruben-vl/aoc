d005be2980a95e413b8c4798eca23bb29d96e94d
ruben-vl <rubenvanlaer@mail.com>
2023-12-04 22:10:26 +0100

    Fix stupidity

--- 2023/python/day4.py
+++ 2023/python/day4.py
@@ -2,9 +2,7 @@ def part1(data: dict[int, tuple[list[int], list[int]]]):
     return sum(0 if len(set(winning) & set(yours)) == 0 else 2**(len(set(winning) & set(yours))-1) for _, (winning, yours) in data.items())
 
 def part2(data: dict[int, tuple[list[int], list[int]]]):
-    card_amount = dict()
-    for i in range(1,len(data.keys())+1):
-        card_amount[i] = 1
+    card_amount = {i: 1 for i in range(1,len(data.keys())+1)}
     for card_number, (winning, yours) in data.items():
         matches = len(set(winning) & set(yours))
         for i in range(card_number+1, min(len(data.keys())+1,card_number+matches+1)):
