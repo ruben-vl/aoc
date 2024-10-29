d18883c5f5c6a8f718776b5d07449f9260cf22f2
ruben-vl <rubenvanlaer@mail.com>
2024-10-29 17:56:21 +0100

    Add 2022 day 6

---
+++ day06_2022.py
@@ -0,0 +1,27 @@
+year = 2022; day = 6
+input_file = f"day{'0'+str(day) if day < 10 else str(day)}_{str(year)}.txt"
+
+def part1():
+    with open(input_file, 'r') as f:
+        input_data = f.read().strip()
+        
+    for i in range(4,len(input_data)):
+        if len(set(input_data[i-4:i])) == 4:
+            return i
+    
+    return None
+
+def part2():
+    with open(input_file, 'r') as f:
+        input_data = f.read()
+        
+    for i in range(14,len(input_data)):
+        if len(set(input_data[i-14:i])) == 14:
+            return i
+    
+    return None
+
+if __name__ == "__main__":
+
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
