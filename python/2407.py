06c86e0f4b6a730ebaca132c978859464f8a536d
ruben-vl <rubenvanlaer@mail.com>
2024-12-07 14:28:08 +0100

    Solve 2024 day 7

---
+++ python/2407.py
@@ -0,0 +1,48 @@
+def get_input_parsed():
+    raw_lines = [line.strip().split(':') for line in open("2407.txt", 'r')]
+    return [(int(agg), [int(e) for e in items.split()]) for agg, items in raw_lines]
+
+def possible_calcs(nums: list[int], intermediate_res: set[int]) -> set[int]:
+    if len(nums) == 0: return intermediate_res
+    new_intermediates = set()
+    next_elem = nums.pop(0)
+    for res in intermediate_res:
+        new_intermediates.add(res + next_elem)
+        new_intermediates.add(res * next_elem)
+    return possible_calcs(nums, new_intermediates)
+
+def part1():
+    data = get_input_parsed()
+    sum_test_values = 0
+    for agg, nums in data:
+        possible_res = possible_calcs(nums[1:], {nums[0]})
+        if agg in possible_res:
+            sum_test_values += agg
+    return sum_test_values
+
+def possible_calcs2(nums: list[int], intermediate_res: set[int]) -> set[int]:
+    if len(nums) == 0: return intermediate_res
+    new_intermediates = set()
+    next_elem = nums.pop(0)
+    for res in intermediate_res:
+        new_intermediates.add(res + next_elem)
+        new_intermediates.add(res * next_elem)
+        new_intermediates.add(int(str(res)+str(next_elem)))
+    return possible_calcs2(nums, new_intermediates)
+
+def part2():
+    data = get_input_parsed()
+    sum_test_values = 0
+    for agg, nums in data:
+        possible_res = possible_calcs2(nums[1:], {nums[0]})
+        # print(agg)
+        # print(nums)
+        # print(possible_res)
+        if agg in possible_res:
+            sum_test_values += agg
+    return sum_test_values
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
